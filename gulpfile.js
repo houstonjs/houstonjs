caef2b018270ec896a970ef0a25e64e2a202df93
Jonathan Birkholz <rookieone@gmail.com>
2014-06-05 13:47:57 -0500

    min css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
 var clean = require('gulp-clean');
 var watch = require('gulp-watch');
+var concat = require('gulp-concat');
 
 gulp.task('clean', function () {
   return gulp.src('build', {read: false}).pipe(clean());
+});
+
+gulp.task('css', function() {
+  return gulp.src(['css/**.*'])
+    .pipe(concat('main.min.css'))
+    .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('move', ['clean'], function(){
@@ -11,7 +18,7 @@ gulp.task('move', ['clean'], function(){
 });
 
 gulp.task('watch', function() {
-  return gulp.watch('contents/**.*', ['move']);
+  return gulp.watch('contents/**.*', ['move', 'css']);
 })
 
-gulp.task('default', ['move']);
+gulp.task('default', ['move', 'css']);
