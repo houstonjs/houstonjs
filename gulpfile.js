561f3b331ee772dc3964f403f6c0cdf2f8e6eccf
Jonathan Birkholz <rookieone@gmail.com>
2014-06-06 22:55:28 -0500

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -14,15 +14,35 @@ gulp.task('css', function() {
     .pipe(gulp.dest('build/css'));
 });
 
+var map = require('map-stream');
+
+var myReporter = map(function (file, cb) {
+  console.log(file);
+  // if (!file.jshint.success) {
+  //   console.log('JSHINT fail in '+file.path);
+  //   file.jshint.results.forEach(function (err) {
+  //     if (err) {
+  //       console.log(' '+file.path + ': line ' + err.line + ', col ' + err.character + ', code ' + err.code + ', ' + err.reason);
+  //     }
+  //   });
+  // }
+  cb(null, file);
+});
+
 gulp.task('news', function () {
     return gulp.src('contents/news/**.md')
         .pipe(markdown())
-        .pipe(gulp.dest('build/news'));
+        .pipe(myReporter);
+        // .pipe(gulp.dest('build/news'));
 });
 
 gulp.task('move', ['clean'], function(){
   return gulp.src(["contents/index.html"], { base: './contents' }).pipe(gulp.dest('build'));
 });
+
+gulp.task('debug', function() {
+  return gulp.watch('gulpfile.js', ['news']);
+})
 
 gulp.task('watch', function() {
   return gulp.watch('contents/**.*', ['move', 'css']);
